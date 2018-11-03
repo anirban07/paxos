5d1bf714075d2e04886c2f4d17a4f1dceb05fb0b
Anirban Biswas <anirban.biswas07@gmail.com>
2018-11-03 14:40:18 -0700

    Fixed indentation

--- src/lspaxos/acceptor.go
+++ src/lspaxos/acceptor.go
@@ -1,63 +1,63 @@
 package lspaxos
 
 import (
-    "errors"
-    "fmt"
-    "net"
-    "net/rpc"
+	"errors"
+	"fmt"
+	"net"
+	"net/rpc"
 )
 
 // Defines an the Acceptor state
 // - Keeps track of a ballot number (highest seen)
 // - Keeps track of a map of previously accepted commands (if any)
 type AcceptorServer struct {
-     // Highest ballot number promised by this acceptor
-     Ballot Ballot
-     
-     // Map to store slot number with commands
-     AcceptedValues map[int]Command
+	// Highest ballot number promised by this acceptor
+	Ballot Ballot
+
+	// Map to store slot number with commands
+	AcceptedValues map[int]Command
 }
 
 // Handler for Scout RPC's
 // Checks if ballot number is higher and updates if necessary
 func (h *AcceptorServer) ExecutePropose(req ScoutRequest, res *ScoutResponse) (err error) {
-     if CompareBallot(req.Ballot, h.Ballot) > 0 {
-     	h.Ballot = req.Ballot
-	// For debug purposes, TODO: Anir, is there a better way to log this?
-	fmt.Printf("Updated ballot %+v, accepted: %+v\n", h.Ballot, h.AcceptedValues)
-     }
+	if CompareBallot(req.Ballot, h.Ballot) > 0 {
+		h.Ballot = req.Ballot
+		// For debug purposes, TODO: Anir, is there a better way to log this?
+		fmt.Printf("Updated ballot %+v, accepted: %+v\n", h.Ballot, h.AcceptedValues)
+	}
 
-     res.Ballot = h.Ballot
-     res.AcceptedValues = h.AcceptedValues
-     return
+	res.Ballot = h.Ballot
+	res.AcceptedValues = h.AcceptedValues
+	return
 }
 
 // Handler for Commander RPC's
 // Only accepts the command sent by the commander if the ballot of the commander is equivalent
 // to the ballot promised by the acceptor (i.e. The Commander is the leader)
 func (h *AcceptorServer) ExecuteAccept(req CommanderRequest, res *CommanderResponse) (err error) {
-     if CompareBallot(req.Ballot, h.Ballot) == 0 {
-     	h.AcceptedValues[req.Slot] = req.Command
-     }
+	if CompareBallot(req.Ballot, h.Ballot) == 0 {
+		h.AcceptedValues[req.Slot] = req.Command
+	}
 
-     res.Ballot = h.Ballot
-     return
+	res.Ballot = h.Ballot
+	return
 }
 
 // Helper to spawn an instance of an Acceptor
 // Example usage: go StartAcceptorServer("Alpha", 1234)
 // Returns an error if unable to set up a listening port
 func StartAcceptorServer(Name string, Port string) (err error) {
-     rpc.Register(&AcceptorServer{AcceptedValues: make(map[int]Command)})
-     fmt.Printf("Acceptor server %s listening on port %s\n", Name, Port)
-     listener, err := net.Listen("tcp", ":"+Port)
-     defer listener.Close()
+	rpc.Register(&AcceptorServer{AcceptedValues: make(map[int]Command)})
+	fmt.Printf("Acceptor server %s listening on port %s\n", Name, Port)
+	listener, err := net.Listen("tcp", ":"+Port)
+	defer listener.Close()
 
-     if err != nil {
-     	err = errors.New("Failed to set up listening port " + Port + " on " + Name)
+	if err != nil {
+		err = errors.New("Failed to set up listening port " + Port + " on " + Name)
+		return
+	}
+
+	rpc.Accept(listener)
 	return
-     }
-
-     rpc.Accept(listener)
-     return
 }
--- src/lspaxos/client.go
+++ src/lspaxos/client.go
@@ -1,110 +1,109 @@
 package lspaxos
 
 import (
-    "bufio"
-    "errors"
-    "fmt"
-    "os"
-    "strings"
-    "time"
+	"bufio"
+	"errors"
+	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 type Client struct {
-    // Unique identifier of the client
-    ClientId int64
-     
-    // Initial request sequence number / message id number
-    // Should start at 0
-    MsgId int
+	// Unique identifier of the client
+	ClientId int64
 
-    // Addresses of the replica servers
-    Replicas []string
+	// Initial request sequence number / message id number
+	// Should start at 0
+	MsgId int
 
-    // Current time out
-    TimeoutMillis int
+	// Addresses of the replica servers
+	Replicas []string
+
+	// Current time out
+	TimeoutMillis int
 }
-
 
 // Starts a client that issues requests in the order
 // of the given specification of lock/unlock operations
 func (c *Client) StartClient(Spec []string) {
-    c.MsgId = 1
-    c.TimeoutMillis = 0
-    done := make(chan interface{}, len(Spec))
-    for _, line := range Spec {
-        parts := strings.Fields(line)
-        command := Command{LockName: parts[1],
-                           LockOp: LockOp(parts[0]),
-                           MsgId: c.MsgId, 
-                           ClientId: c.ClientId}
+	c.MsgId = 1
+	c.TimeoutMillis = 0
+	done := make(chan interface{}, len(Spec))
+	for _, line := range Spec {
+		parts := strings.Fields(line)
+		command := Command{LockName: parts[1],
+			LockOp:   LockOp(parts[0]),
+			MsgId:    c.MsgId,
+			ClientId: c.ClientId}
 
-        // Send the command to each replica
-        c.SendCommand(command, done)
-        
-        // Grab responses, break on the first valid one.
-        for {
-            response := <- done
-            if response == false {
-                fmt.Println("Error occurred in the RPC, break")
-                panic(errors.New("Failed to call replica, invariant broken"))
-            }
+		// Send the command to each replica
+		c.SendCommand(command, done)
 
-            var resp = response.(ClientResponse)
-            if resp.MsgId != c.MsgId {
-                // Stale message
-                continue
-            }
+		// Grab responses, break on the first valid one.
+		for {
+			response := <-done
+			if response == false {
+				log.Println("Error occurred in the RPC, break")
+				panic(errors.New("Failed to call replica, invariant broken"))
+			}
 
-            if resp.Err == ErrLockHeld {
-                // Add constant amount to timeout
-                // Wait, then resend commands
-                // Have to increment the message id to deal with stale responses
-                c.TimeoutMillis += 500
-                time.Sleep(time.Duration(c.TimeoutMillis) * time.Millisecond)
-                c.MsgId++
-                c.SendCommand(command, done)
-                continue
-            } else if resp.Err == OK {
-                fmt.Printf("Client %d successfully executed %+v\n", c.ClientId, command)
-            }
+			var resp = response.(ClientResponse)
+			if resp.MsgId != c.MsgId {
+				// Stale message
+				continue
+			}
 
-            // Either way, we're here if the lock didn't exist
-            // or if the command succeeded. Need to exit and then decrease the
-            // timeout by a factor  of 2
-            c.TimeoutMillis /= 2
-            break
-        }
+			if resp.Err == ErrLockHeld {
+				// Add constant amount to timeout
+				// Wait, then resend commands
+				// Have to increment the message id to deal with stale responses
+				c.TimeoutMillis += 500
+				time.Sleep(time.Duration(c.TimeoutMillis) * time.Millisecond)
+				c.MsgId++
+				c.SendCommand(command, done)
+				continue
+			} else if resp.Err == OK {
+				log.Printf("Client %d successfully executed %+v\n", c.ClientId, command)
+			}
 
-        // Start the next message
-        c.MsgId++
-    }
-    close(done)
+			// Either way, we're here if the lock didn't exist
+			// or if the command succeeded. Need to exit and then decrease the
+			// timeout by a factor  of 2
+			c.TimeoutMillis /= 2
+			break
+		}
+
+		// Start the next message
+		c.MsgId++
+	}
+	close(done)
 }
 
 // Send a command to every replica asynchronously
 func (c *Client) SendCommand(Command Command, Done chan interface{}) {
-    for _, server := range c.Replicas {
-        request := ClientRequest{Command: Command}
-        response := new(ClientResponse)
-        go Call(server, "ReplicaServer.ExecuteRequest", request, response, Done)
-    }
+	for _, server := range c.Replicas {
+		request := ClientRequest{Command: Command}
+		response := new(ClientResponse)
+		go Call(server, "ReplicaServer.ExecuteRequest", request, response, Done)
+	}
 }
 
 // Read the specification for the client from a file
 func ReadSpec(Filename string) []string {
-    fd, err := os.Open(Filename)
-    if err != nil {
-        fmt.Println("Error opening file " + Filename)
-        panic(err)
-    }
-    defer fd.Close()
+	fd, err := os.Open(Filename)
+	if err != nil {
+		log.Println("Error opening file " + Filename)
+		panic(err)
+	}
+	defer fd.Close()
 
-    reader := bufio.NewScanner(fd)
-    spec := make([]string, 0)
-    
-    for reader.Scan() {
-        spec = append(spec, reader.Text())
-    }
+	reader := bufio.NewScanner(fd)
+	spec := make([]string, 0)
 
-    return spec
+	for reader.Scan() {
+		spec = append(spec, reader.Text())
+	}
+
+	return spec
 }
