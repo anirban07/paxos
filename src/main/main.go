e64bdda40f2bbdd903ba483b62df94e6cf1d1137
Robbitron <ram16@cs.uw.edu>
2018-11-05 17:30:51 -0800

    Made test file for testing paxos

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"lspaxos"
+	"strings"
+)
+
+const (
+	ip = "localhost:"
 )
 
 func main() {
-	lspaxos.StartAcceptorServer(0, "1234")
+	AcceptorAddrs := []string{ip + "10000", ip + "10001", ip + "10002"}
+	LeaderAddrs := []string{ip + "20000", ip + "20001"}
+	ReplicaAddrs := []string{ip + "30000"}
+
+	// Start the acceptors
+	for id, addr := range AcceptorAddrs {
+		go lspaxos.StartAcceptor(int64(id), strings.Split(addr, ":")[1])
+	}
+
+	// Start the leaders
+	for id, addr := range LeaderAddrs {
+		go lspaxos.StartLeader(id, AcceptorAddrs, strings.Split(addr, ":")[1])
+	}
+
+	// Start the replicas
+	for id, addr := range ReplicaAddrs {
+		go lspaxos.StartReplica(int64(id), LeaderAddrs, strings.Split(addr, ":")[1])
+	}
+
+	Spec := lspaxos.ReadSpec("src/specs/test_spec.txt")
+	lspaxos.StartClient(int64(666), ReplicaAddrs, Spec)
 }
